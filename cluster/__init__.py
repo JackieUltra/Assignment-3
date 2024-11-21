b33482a3ed837181cbfa382ae28687767624074b
JackieUltra <jbsands78@gmail.com>
2024-11-20 22:57:31 -0800

    init

--- cluster/__init__.py
+++ cluster/__init__.py
@@ -1,2 +1,7 @@
-from .kmeans import KMeans
-#from .silhouette import Silhouette
+"""
+CDD 203 - Assingment 3
+"""
+from kmeans import KMeans
+from visualization import plot_3d_clusters
+
+__version__ = "1.0.0"
